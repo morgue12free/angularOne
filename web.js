c4ee9afeb7fe817a452a9227ee3a8cd1e0652d04
Etinosa <em.obanor@gmail.com>
2015-11-17 04:48:52 +0000

    Deployment

--- web.js
+++ web.js
@@ -3,5 +3,4 @@ var gzippo = require('gzippo');
   var app = express();
  
   app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
   app.listen(process.env.PORT || 8000);
