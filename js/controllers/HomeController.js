81b589a5ed90c5d27811652474ff62def1f36144
Etinosa <em.obanor@gmail.com>
2015-11-17 03:59:47 +0000

    Complete and ready to deploy

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -3,6 +3,15 @@
 
 app.controller('HomeController',['$scope','stream', function($scope, stream) {
     stream.then(function(data) {
-        $scope.photos = data;
+        $scope.photos = data
       });  
+        //show more functionality
+
+       $scope.showMoreItems = function(data) {
+         $scope.photos = $scope.photos + data;
+         $route.reload();
+           $scope.photos = $scope.photos.push(data);
+       }; 
+
+
 }]);
